9af91dad94b36a07767e208af5ced78c281d7769
Collin McRann <cmcrann2@yahoo.com>
2021-07-02 21:04:18 -0400

    Create data.rs
    parse basic data from registers

---
+++ src/data.rs
@@ -0,0 +1,47 @@
+pub fn test01(dt: &str,ar02:&str,rsp: &mut [u16], rsp2: &mut [u16],rsp3: &mut [u16],) {
+
+if ar02 == "-d" {
+            println!("{},{:?},{:?},{:?},{:?},{:?},{:?},{:?},{:?},{:?},{:?},{:?},{:?},{:?},{:?},{:?}",dt,rsp[0] as f32, 0.01 * rsp[1] as f32, 0.01 * rsp2[0] as f32,0.01 * rsp3[16] as f32,0.01 * rsp3[17] as f32, 0.01 * rsp3[18] as f32,0.01 * rsp3[12] as f32,0.01 * rsp3[13] as f32,0.01 * rsp3[14] as f32,0.01 * rsp3[2] as f32,0.01 * rsp3[1] as f32,0.01 * rsp3[0] as f32,0.01 * rsp3[4] as f32,0.01 * rsp3[5] as f32,0.01 * rsp3[6] as f32);
+            }
+            else if ar02 == "-dh" {
+            println!("Time,BatterySOC,Remote_battery_temperature,battery_real_rated_power,battery_temp,temperature_inside_equipment,power_components_temperature,Discharging_equipment_output_voltage,Discharging_equipment_output_current,Discharging_equipment_output_power,Charging_equipment_input_power,Charging_equipment_input_current,Charging_equipment_input_voltage,Charging_equipment_output_voltage,Charging_equipment_output_current,Charging_equipment_output_power");
+            println!("{},{:?},{:?},{:?},{:?},{:?},{:?},{:?},{:?},{:?},{:?},{:?},{:?},{:?},{:?},{:?}",dt,rsp[0] as f32, 0.01 * rsp[1] as f32, 0.01 * rsp2[0] as f32,0.01 * rsp3[16] as f32,0.01 * rsp3[17] as f32, 0.01 * rsp3[18] as f32,0.01 * rsp3[12] as f32,0.01 * rsp3[13] as f32,0.01 * rsp3[14] as f32,0.01 * rsp3[2] as f32,0.01 * rsp3[1] as f32,0.01 * rsp3[0] as f32,0.01 * rsp3[4] as f32,0.01 * rsp3[5] as f32,0.01 * rsp3[6] as f32);
+            }
+            else if ar02 == "-r" {
+
+                println!("Basic EPSolar Tracer system info:");
+        println!("================================");
+        println!("Battery SOC: {:?}", rsp[0] as f32);
+        println!("Remote battery temperature: {:?}",0.01 * rsp[1] as f32);
+        println!("================================");
+        println!("Battery's real rated power: {:?}", 0.01 *rsp2[0] as f32);
+        println!("================================");
+        println!("Battery Temperature: {:?}",0.01 * rsp3[16] as f32);
+        println!("Temperature inside equipment: {:?}",0.01 * rsp3[17] as f32);
+        println!("Power components temperature: {:?}",0.01 * rsp3[18] as f32);
+        println!("================================");
+        println!("Discharging equipment output voltage:: {:?}",0.01 * rsp3[12] as f32);
+        println!("Discharging equipment output current: {:?}",0.01 * rsp3[13] as f32);
+        println!("Discharging equipment output power: {:?}",0.01 * rsp3[14] as f32);
+        println!("================================");
+        println!("Charging equipment input power:{:?}",0.01 * rsp3[2] as f32);
+        println!("Charging equipment input current: {:?}",0.01 * rsp3[1] as f32);
+        println!("Charging equipment input voltage: {:?}",0.01 * rsp3[0] as f32);
+        println!("================================");
+        println!("Charging equipment output voltage: {:?}",0.01 * rsp3[4] as f32);
+        println!("Charging equipment output current: {:?}",0.01 * rsp3[5] as f32);
+        println!("Charging equipment output power: {:?}",0.01 * rsp3[6] as f32);
+
+            }
+            else {
+                println!("Argument not understood");
+                println!("Available options");
+                println!("-r Read mode");
+                println!("-dh CSV header mode");
+                println!("-d CSV no header mode");
+            }
+
+
+
+
+}
